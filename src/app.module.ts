d74eaa6b9f1811eee65a1fcb0cee5bfddacc30f4
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-09 21:22:37 -0500

    Add: resolver hello-world

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      // playground: false,
       autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
     }),
+    HelloWorldModule,
   ],
   controllers: [],
   providers: [],
