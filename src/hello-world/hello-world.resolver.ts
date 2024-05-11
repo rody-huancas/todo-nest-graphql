de34d799b690dc2f8bcecd7b99d0e39fb6ec14b4
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-10 20:24:06 -0500

    Add: examples with graphql

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
+  @Query(() => String, { description: 'Hola mundo retornado', name: 'Holi' })
+  helloWorld(): string {
+    return 'Hola Mundo';
+  }
 
-    @Query( () => String )
-    helloWorld(): string {
-        return 'Hola Mundo'
-    }
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, { name: 'getRandomFromZeroTo' })
+  getRandomFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to: number = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
+  }
 }
